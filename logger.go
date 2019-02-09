c38f23627d2ee4efd7cf892daa0a8ef5341ea33a
greatbridf <guobingfang20040616@gmail.com>
2019-02-09 12:23:04 +0800

    接收log文件名作为参数 自动打开关闭文件

--- logger.go
+++ logger.go
@@ -2,26 +2,28 @@ package logger
 
 import (
 	"fmt"
-	"io"
+	"os"
 	"time"
 )
 
 type Logger struct {
-	prefix string
-	out    io.Writer
+	prefix   string
+	filename string
 }
 
-func New(out io.Writer, prefix string) *Logger {
+func New(filename string, prefix string) *Logger {
 	return &Logger{
-		out:    out,
-		prefix: prefix,
+		filename: filename,
+		prefix:   prefix,
 	}
 }
 
 func (log Logger) Write(msg string, TYPE string) {
-	now := time.Now().UTC().Format("2006-01-02 UTC 15:04:05")
+	now := time.Now().UTC().Format("2006-01-02 UTC 15:04:05.000")
 	out := fmt.Sprintf("[%s] %s [%s] %s\n", log.prefix, now, TYPE, msg)
-	log.out.Write([]byte(out))
+	file, _ := os.OpenFile(log.filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	defer file.Close()
+	file.Write([]byte(out))
 }
 
 func (log Logger) D(msg string) {
